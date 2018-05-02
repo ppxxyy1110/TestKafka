ef3310897cad9f136eea854c4404871fa0282d69
ppxxyy1110 <334067072@qq.com>
2018-05-02 19:57:04 +0800

    intial

---
+++ Testkafkaconsumer/src/main/java/com/testkafka/testkafka/TestkafkaApplication.java
@@ -0,0 +1,12 @@
+package com.testkafka.testkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestkafkaApplication.class, args);
+	}
+}
